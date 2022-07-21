eb214811af80f7d433152c6c7d1877cb035ebcb0
rkddbs1031 <rkddbs1031@gmail.com>
2022-07-21 17:47:52 +0900

    ADD: style setting

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 import App from './routes'
+import './styles'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
