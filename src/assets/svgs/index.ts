079c1f1b492b23807a07ad145ca0c708b4b3ec08
rkddbs1031 <rkddbs1031@gmail.com>
2022-07-21 18:48:21 +0900

    ADD: svg Icon

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as EditIcon } from './edit.svg'
+export { ReactComponent as DeleteIcon } from './delete.svg'
+export { ReactComponent as ModifyIcon } from './modify.svg'
+export { ReactComponent as CancleIcon } from './cancle.svg'
