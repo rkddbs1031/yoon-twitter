d83785f9783e2893082a2b09448210219f93874b
rkddbs1031 <rkddbs1031@gmail.com>
2022-07-25 17:13:59 +0900

    MOD: image file upload

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,10 +1,12 @@
 import { FormEvent, ChangeEvent, useRef } from 'react'
 import { useRecoilValue } from 'recoil'
+import { v4 as uuidv4 } from 'uuid'
 
 import { useState, useEffect } from 'hooks'
 import { userObjstate } from 'states'
 import { IYweetsData } from 'types/yweets'
-import { dbService } from 'utils/firebase'
+import { dbService, storageService } from 'utils/firebase'
+import { ref, uploadString, getDownloadURL } from 'firebase/storage'
 import { addDoc, collection, orderBy, query, onSnapshot } from 'firebase/firestore'
 
 import Yweet from 'components/Yweet'
@@ -14,18 +16,27 @@ import styles from './home.module.scss'
 const Home = () => {
   const [yweet, setYweet] = useState<string>('')
   const [yweets, setYweets] = useState<IYweetsData[]>([])
-  const [fileURL, setFileURL] = useState<string>()
+  const [imageurl, setImageURL] = useState<string>('')
   const userObj = useRecoilValue(userObjstate)
   const fileRef = useRef<HTMLInputElement>(null)
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    // 사진먼저 업로드하기
+    let getFileURL = ''
+    const storageRef = ref(storageService, `${userObj.uid}/${uuidv4()}`)
+    if (imageurl) {
+      const res = await uploadString(storageRef, imageurl, 'data_url')
+      getFileURL = await getDownloadURL(res.ref)
+    }
     await addDoc(collection(dbService, 'yweets'), {
       yweet,
       createdAt: Date.now(),
       creatorId: userObj.uid,
+      imageurl: getFileURL,
     }).then(() => {
       setYweet('')
+      handleCancleUpload()
     })
   }
 
@@ -47,18 +58,19 @@ const Home = () => {
       const uploadFile = files[0]
       const reader = new FileReader()
       reader.onload = (finishedEvent) => {
-        setFileURL(finishedEvent.target?.result as string)
+        setImageURL(finishedEvent.target?.result as string)
       }
       reader.readAsDataURL(uploadFile)
     }
   }
 
   const handleCancleUpload = () => {
-    setFileURL('')
+    setImageURL('')
     if (fileRef.current) {
       fileRef.current.value = ''
     }
   }
+
   useEffect(() => {
     getYweets()
   }, [])
@@ -70,10 +82,10 @@ const Home = () => {
         <div className={styles.inputBox}>
           <input type='text' value={yweet} placeholder="what's on your mind?" maxLength={120} onChange={handleChange} />
           <input type='file' accept='image/*' onChange={onFileChange} ref={fileRef} />
-          {fileURL && (
+          {imageurl && (
             <>
               <div className={styles.imgWrap}>
-                <img src={fileURL} alt='test' />
+                <img src={imageurl} alt='test' />
               </div>
               <button type='button' onClick={handleCancleUpload}>
                 삭제
