1807b8d1a730e57572c02992482b1c4ac382ee5a
rkddbs1031 <rkddbs1031@gmail.com>
2022-07-25 23:53:37 +0900

    FIX: 로그인 시 바로 home으로 안가는 오류 수정

--- src/routes/Auth/index.tsx
+++ src/routes/Auth/index.tsx
@@ -1,13 +1,19 @@
-import { MouseEvent } from 'react'
+import { MouseEvent, useEffect } from 'react'
+import { useRecoilValue } from 'recoil'
+
 import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { userObjstate } from 'states'
+import { IUserInfo } from 'types/yweets'
 
 import AuthForm from './AuthForm'
 import { TweetIcon, GoogleIcon, GithubIcon } from 'assets/svgs'
-
 import styles from './auth.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 const AuthPage = () => {
+  const setUserObj = useRecoilValue<IUserInfo>(userObjstate)
   const AUTH = getAuth()
+  const navigate = useNavigate()
 
   const handleSocialLogin = async (e: MouseEvent<HTMLButtonElement>) => {
     const { name } = e.currentTarget
@@ -15,6 +21,10 @@ const AuthPage = () => {
     name === 'google' ? (provider = new GoogleAuthProvider()) : (provider = new GithubAuthProvider())
     await signInWithPopup(AUTH, provider)
   }
+
+  useEffect(() => {
+    setUserObj.uid && navigate('/')
+  }, [navigate, setUserObj])
 
   return (
     <section className={styles.authContainer}>
