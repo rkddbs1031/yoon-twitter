480d0d8e893d360b80917da98e6eb264cec7a39a
rkddbs1031 <rkddbs1031@gmail.com>
2022-07-25 23:53:56 +0900

    MOD: 로그인 했을 경우만 헤더 나타나게 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, useNavigate } from 'react-router-dom'
-import { useSetRecoilState } from 'recoil'
+import { useRecoilState, useSetRecoilState } from 'recoil'
 
 import { useState, useEffect } from 'hooks'
 import { authService } from 'utils/firebase'
@@ -16,8 +16,8 @@ import styles from './routes.module.scss'
 
 const App = () => {
   const [ready, setReady] = useState<boolean>(false)
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true)
-  const setUserObj = useSetRecoilState<IUserInfo>(userObjstate)
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+  const [userObj, setUserObj] = useRecoilState<IUserInfo>(userObjstate)
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -45,11 +45,10 @@ const App = () => {
 
   if (!ready) return <Loading />
 
-  // if (init && )
   return (
     <div className={styles.appWrap}>
       <div className={styles.app}>
-        <Header />
+        {userObj.uid && <Header />}
         <main>
           <Routes>
             <Route path='/' element={<Home />} />
