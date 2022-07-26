a740b20059e9bcd1d5dbc2e07c02b5ab81f2a6e7
rkddbs1031 <rkddbs1031@gmail.com>
2022-07-27 00:03:11 +0900

    MOD: Header component style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,18 @@
 import { NavLink } from 'react-router-dom'
 import { NAVI_ITEM } from 'utils/navigation'
+import { cx } from 'styles'
+import styles from './header.module.scss'
 
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <ul>
         {NAVI_ITEM.map((item) => (
           <li key={item.id}>
-            <NavLink to={item.href}>{item.text}</NavLink>
+            <NavLink to={item.href} className={({ isActive }) => cx({ [styles.isActive]: isActive })}>
+              <item.svg />
+              <span>{item.text}</span>
+            </NavLink>
           </li>
         ))}
       </ul>
