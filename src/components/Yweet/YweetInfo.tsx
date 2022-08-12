bd301ba210de6b3e3965868e45e303315dc44f34
rkddbs1031 <rkddbs1031@gmail.com>
2022-08-12 18:35:13 +0900

    MOD:글 작성 시간 추가

--- src/components/Yweet/YweetInfo.tsx
+++ src/components/Yweet/YweetInfo.tsx
@@ -8,25 +8,31 @@ interface IProps {
 const YweetInfo = ({ yweetObj }: IProps) => {
   return (
     <dl>
-      {yweetObj.imageurl && (
-        <div className={styles.userImg}>
-          <dt>이미지</dt>
-          <dd>
-            <div className={styles.imgContainer}>
-              <img src={yweetObj.imageurl} alt={yweetObj.yweet} />
-            </div>
-          </dd>
+      <div className={styles.yweetTop}>
+        {yweetObj.imageurl && (
+          <div className={styles.userImg}>
+            <dt>이미지</dt>
+            <dd>
+              <div className={styles.imgContainer}>
+                <img src={yweetObj.imageurl} alt={yweetObj.yweet} />
+              </div>
+            </dd>
+          </div>
+        )}
+        <div className={styles.userInfo}>
+          {yweetObj.userId && (
+            <>
+              <dt>아이디</dt>
+              <dd className={styles.userId}>{yweetObj.userId}</dd>
+            </>
+          )}
+          <dt>시간</dt>
+          <dd className={styles.userDate}>{yweetObj.createdAt}</dd>
         </div>
-      )}
-      <div>
+      </div>
+      <div className={styles.yweetBottom}>
         <dt>yweet</dt>
         <dd className={styles.yweetDesc}>{yweetObj.yweet}</dd>
-        {yweetObj.userId && (
-          <>
-            <dt>아이디</dt>
-            <dd className={styles.userId}>{yweetObj.userId}</dd>
-          </>
-        )}
       </div>
     </dl>
   )
